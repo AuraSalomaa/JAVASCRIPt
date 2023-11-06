d92742fa5e2778aa6aab4efe2abd6dde0fbdadc4
AuraSalomaa <aura@salomaa.info>
2023-11-06 10:39:56 +0200

    tehtävä1.js tehty

---
+++ JS_tehtävät/Tehtävä3.js
@@ -0,0 +1,15 @@
+const Number1 = prompt("give the first number");
+const Number2 = prompt("give the second number");
+const Number3 = prompt("give the third number")
+const first = parseInt(Number1);
+const second = parseInt(Number2);
+const third = parseInt(Number3);
+
+sum = first + second + third
+product = first * second * third
+avarage = first + second + third / 3
+console.log(sum)
+console.log(product)
+console.log(avarage)
+
+
