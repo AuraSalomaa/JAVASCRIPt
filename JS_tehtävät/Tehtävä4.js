ccd3012c50263048af327862d756123e5f6d656b
AuraSalomaa <aura@salomaa.info>
2023-11-06 11:04:51 +0200

    tehtävä1.js tehty

---
+++ JS_tehtävät/Tehtävä4.js
@@ -0,0 +1,12 @@
+
+const sorting_hat = Math.floor(Math.random() * 5);
+if (sorting_hat === 1) {
+    console.log("You are a Gryfindor")
+}else if (sorting_hat === 2){
+    console.log("You are a Ravenclaw")
+}else if (sorting_hat === 3) {
+    console.log("You are a Slytherin")
+
+} else if (sorting_hat === 4) {
+    console.log("You are an Hufflepuff")
+}
