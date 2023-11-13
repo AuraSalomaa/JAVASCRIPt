b5bd51c88e2c66f8783d3fc4a1136d9d1b5e5577
AuraSalomaa <aura@salomaa.info>
2023-11-13 20:52:54 +0200

    turilainen

---
+++ JS_Tehtävät2/tehtävä_2/tehtävä2.js
@@ -0,0 +1,15 @@
+
+const amount = prompt("How many particapents?")
+let jasenia = []
+const maara = parseInt(amount)
+const OrgList = []
+const kysynta = ""
+for (let i = 0; i < maara;i++) {
+    const kysynta = prompt("write names of the particapents?")
+    jasenia.push(kysynta)
+
+}
+document.getElementById("skripti").innerHTML =" jasenia"
+
+
+
