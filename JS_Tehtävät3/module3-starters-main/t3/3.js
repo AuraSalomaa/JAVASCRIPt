1362897bde0542d812e35a9ce57ccc67d1f474dd
AuraSalomaa <aura@salomaa.info>
2023-11-19 10:06:24 +0200

    turilainen

--- JS_Tehtävät3/module3-starters-main/t3/3.js
+++ JS_Tehtävät3/module3-starters-main/t3/3.js
@@ -1,5 +1,9 @@
 'use strict';
-const names = ['John', 'Paul', 'Jones'];
-for (let i = 0; i > names.length; i++) {
-
+let name = ""
+const names= ['John', 'Paul', 'Jones'];
+for (let i = 0; i < names.length; i++) {
+    name +=  "<li>" +  names[i] + "</li>"
+    console.log(names[i])
 }
+
+document.querySelector("#target").innerHTML = name
--- JS_tehtävät/Tehtävä_4/Tehtävä4.js
+++ JS_tehtävät/Tehtävä_4/Tehtävä4.js
@@ -1,12 +1,14 @@
-
+var hat = ""
 const sorting_hat = Math.floor(Math.random() * 5);
 if (sorting_hat === 1) {
-    console.log("You are a Gryfindor")
+
+    document.querySelector("#JSskirpti4").innerHTML =  " You are a Gryfindor"
 }else if (sorting_hat === 2){
-    console.log("You are a Ravenclaw")
+    document.querySelector("#JSskripti4").innerHTML = " You are a Ravenclaw"
 }else if (sorting_hat === 3) {
-    console.log("You are a Slytherin")
+    document.querySelector("#JSskripti4").innerHTML = " You are a Slytherin"
 
 } else if (sorting_hat === 4) {
-    console.log("You are an Hufflepuff")
+    document.querySelector("#JSskripti4").innerHTML = " You are an Hufflepuff"
 }
+
