2fe5ba181f45f467797fabb71371c14002cb123c
AuraSalomaa <aura@salomaa.info>
2023-11-19 16:44:17 +0200

    turilainen

--- JS_Tehtävät3/module3-starters-main/t7/7.js
+++ JS_Tehtävät3/module3-starters-main/t7/7.js
@@ -1,4 +1,8 @@
-function change () {
-    if mou("#target")
+let vaihto = document.querySelector("#target")
+vaihto.addEventListener('mouseover', function (){
+    vaihto.src = "img/picB.jpg"
+})
 
-}
+vaihto.addEventListener("mouseout", function (){
+    vaihto.src = "img/picA.jpg"
+})
