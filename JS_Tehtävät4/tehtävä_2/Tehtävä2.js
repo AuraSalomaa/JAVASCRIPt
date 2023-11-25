d67e0e7201821df7c0af74e16c506d14b36725bb
AuraSalomaa <aura@salomaa.info>
2023-11-25 19:23:33 +0200

    turilainen

--- JS_Tehtävät4/tehtävä_2/Tehtävä2.js
+++ JS_Tehtävät4/tehtävä_2/Tehtävä2.js
@@ -3,13 +3,13 @@ tvshowform.addEventListener('submit', async function(evt){
         evt.preventDefault();
 
         const code = document.querySelector('input[name]').value;
-
+        console.log(code)
         try{
             const response = await fetch(`https://api.tvmaze.com/search/shows/${code}`);
             const jsonData = await response.json();
-            console.log(jsonData['value'])
-            console.log(response)
-            console.log(code)
+            console.log(jsonData["id"]["show"]["name"])
+            console.log()
+
 
 
 
