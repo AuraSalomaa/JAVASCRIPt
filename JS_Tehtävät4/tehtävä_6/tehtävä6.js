38fd5fd37f0008b5c66175147fdbadfaf5e7f341
AuraSalomaa <aura@salomaa.info>
2023-11-30 14:10:54 +0200

    tehtävä1.js tehty

--- JS_Tehtävät4/tehtävä_6/tehtävä6.js
+++ JS_Tehtävät4/tehtävä_6/tehtävä6.js
@@ -1,21 +1,28 @@
 'use strict';
 
         const Vitsit = document.querySelector('#vitsikysely')
+        const vitsi = document.getElementById("jokes")
             Vitsit.addEventListener('submit', async function(evt) {
 
             evt.preventDefault();
 
             const code = document.querySelector('input[name=query]').value;
             try {
+                let thing = ""
                 const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${code}`);
                 const jsonData = await response.json();
-                vastaus = response.value
-                return jsonData
+                jsonData.result.forEach(joke => {
+                    const article = document.createElement('article')
+                    const p = document.createElement('p')
+                    p.textContent = joke.value
+                    article.appendChild(p)
+
+                    thing = vitsi.appendChild(article)
+                    document.querySelector("#jokes").innerHTML = thing
+                })
 
 
             }catch (error) {
                 console.log(error.message);
         }
     });
-
-    vastaus = Vitsit()
