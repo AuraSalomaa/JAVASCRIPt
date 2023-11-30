cbb211755c69d28e7afb0a5aae96229a056787bb
AuraSalomaa <aura@salomaa.info>
2023-11-30 09:50:26 +0200

    testi

--- JS_Tehtävät4/tehtävä_2/tehtävä2.js
+++ JS_Tehtävät4/tehtävä_2/tehtävä2.js
@@ -1,1 +1,18 @@
 
+'use strict';
+    const requestForm = document.querySelector('#requestForm')
+    requestForm.addEventListener('submit', async function(evt) {
+
+
+        evt.preventDefault()
+
+        const name = document.querySelector('input[name=q]').value;
+
+        try {
+            const response = await fetch(`https://api.tvmaze.com/search/shows?q=${name}`)
+            const jsonData = await response.json()
+            console.log(jsonData)
+        }catch (error) {
+            console.log(error.message);
+            }
+    });
